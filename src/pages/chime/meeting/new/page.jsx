e77f44b0c9ecdcb05149bba13c69ffcbf8dc92a9
leejay <leejay@gemvaxlink.com>
2025-01-19 11:03:00 +0900

    toggle local video working

--- src/pages/chime/meeting/new/page.jsx
+++ src/pages/chime/meeting/new/page.jsx
@@ -6,12 +6,10 @@ import {
 import * as AWS from 'aws-sdk'
 import { useState } from 'react'
 import {
-  CameraSelection,
+  DeviceLabels,
   LocalVideo,
   useLocalVideo,
   useMeetingManager,
-  useVideoInputs,
-  VideoTile,
   VideoTileGrid,
 } from 'amazon-chime-sdk-component-library-react'
 import { MeetingSessionConfiguration } from 'amazon-chime-sdk-js'
@@ -20,19 +18,13 @@ export const NewMeetingPage = () => {
   const [meetingResponse, setMeetingResponse] = useState(null)
   const [attendeeResponse, setAttendeeResponse] = useState(null)
   const [meetingId, setMeetingId] = useState('')
+  const { toggleVideo } = useLocalVideo()
 
   const meetingManager = useMeetingManager()
-  const { tileId, isVideoEnabled, toggleVideo } = useLocalVideo()
-
-  const { devices, selectedDevice } = useVideoInputs()
-  const items = devices.map((device) => (
-    <li key={device.deviceId}>{device.label}</li>
-  ))
 
   const client = new ChimeSDKMeetingsClient({
     region: 'ap-northeast-2',
     credentials: new AWS.Credentials(
-      // TODO: AWS credentials 수정 필요
       '*',
       '*',
     ),
@@ -73,40 +65,17 @@ export const NewMeetingPage = () => {
       meetingResponse.Meeting,
       attendeeResponse.Attendee,
     )
-
-    const deviceLabels = async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true })
-      return stream
-    }
-
     const options = {
-      deviceLabels,
+      deviceLabels: DeviceLabels.AudioAndVideo,
     }
+
+    meetingManager.invokeDeviceProvider(DeviceLabels.AudioAndVideo)
 
     await meetingManager.join(meetingSessionConfiguration, options)
-
-    // TODO: device 관련 설정 수정 필요!
-
-    const videoDevices = await meetingManager.audioVideo.listVideoInputDevices()
-    console.log('videoDevices: ', videoDevices)
-
-    await meetingManager.audioVideo.startVideoInput(videoDevices[0].deviceId)
-
-    const localVideoTileNumber = meetingManager.audioVideo.startLocalVideoTile()
-    console.log('localVideoTileNumber: ', localVideoTileNumber)
-
-    meetingManager.audioVideo.bindVideoElement(
-      localVideoTileNumber,
-      document.getElementById('localVideoTile'),
-    )
-
-    const localVideoTile = meetingManager.audioVideo.getLocalVideoTile()
-    console.log('localVideoTile: ', localVideoTile)
-
     await meetingManager.start()
   }
 
-  const stopMeeting = async () => {
+  const handleStopMeeting = async () => {
     await meetingManager.leave()
   }
 
@@ -116,34 +85,9 @@ export const NewMeetingPage = () => {
       <button onClick={handleCreateMeeting}>Create Meeting</button>
       <button onClick={handleCreateAttendee}>Create Attendee</button>
       <button onClick={handleJoinMeeting}>Join Meeting</button>
-      <CameraSelection />
-      <LocalVideo active={true} />
-      <VideoTile
-        id={'localVideoTile'}
-        style={{
-          border: '1px solid grey',
-          gridArea: '',
-          width: '300px',
-          height: '200px',
-        }}
-        nameplate="Tile 1"
-        tileId={1}
-        active={true}
-      />
-      <VideoTileGrid />
-      <p>Tile ID: {tileId}</p>
-      <p>
-        {isVideoEnabled ? 'LocalVideo is enabled' : 'LocalVideo is disabled'}
-      </p>
-      <button onClick={toggleVideo}>Toggle video</button>
-      <button onClick={stopMeeting}>Stop Meeting</button>
-      <div>
-        <p>
-          Current Selected Video Input Device: {JSON.stringify(selectedDevice)}
-        </p>
-        <p>Devices</p>
-        <ul>{items}</ul>
-      </div>
+      <button onClick={handleStopMeeting}>Stop Meeting</button>
+      <LocalVideo style={{ width: '500px' }} />
+      <button onClick={toggleVideo}>toggle video</button>
     </>
   )
 }
